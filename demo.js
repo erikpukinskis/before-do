9fdc603e8d09fddc994affeb951ae91523bda3ae
Erik Pukinskis <erik@snowedin.net>
2017-05-01 10:05:43 -0700

    carriage returns

--- demo.js
+++ demo.js
@@ -1,21 +1,41 @@
 
-beforeDo("the day is over", "climb into your body")
+beforeDo(
+  "the day is over",
+  "climb into your body")
 
-beforeDo("smoking", "go outside. take three deep breaths.")
+beforeDo(
+  "smoking",
+  "go outside. take three deep breaths.")
 
-beforeDo("eating some junk", "drink a glass of water")
+beforeDo(
+  "eating some junk",
+  "drink a glass of water")
 
-beforeDo("let anxiety destroy your life", "boredom OR meditation OR time travel", timeTravel)
+beforeDo(
+  "let anxiety destroy your life",
+  "boredom OR meditation OR time travel", timeTravel)
 
-beforeDo.instruction("time travel", "close your eyes and let your mind do whatever it wants. doing things in imaginary land can increase your drive to make things happen in real life. Doing fiddly things burns up your juice instead.")
+beforeDo.instruction(
+  "time travel",
+  "close your eyes and let your mind do whatever it wants. doing things in imaginary land can increase your drive to make things happen in real life. Doing fiddly things burns up your juice instead.")
 
-beforeDo("procrastinating even more", "connect with your values OR treat yourself for anxiety OR nap/etc/selfcare if you are not feeling physically ready to go")
+beforeDo(
+  "procrastinating even more",
+  "connect with your values OR treat yourself for anxiety OR nap/etc/selfcare if you are not feeling physically ready to go")
 
-beforeDo("spending more time spinning endlessly in your head", "sustained physically work")
+beforeDo(
+  "spending more time spinning endlessly in your head",
+  "sustained physically work")
 
-beforeDo("thinking on rails", "smoke pot")
+beforeDo(
+  "thinking on rails",
+  "smoke pot")
 
-beforeDo("binge eating", "drink water OR cook a real meal OR eat a real meal OR go to sleep")
+beforeDo(
+  "binge eating",
+  "drink water OR cook a real meal OR eat a real meal OR go to sleep")
 
-beforeDo("eating out", "imagine what would be the perfect thing to cook right now")
+beforeDo(
+  "eating out",
+  "imagine what would be the perfect thing to cook right now")
 
